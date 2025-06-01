49c399d4d8eb816b9fbf3940a6f7177852200c7b
leandro <leandro@pop-os.localdomain>
2025-05-31 23:15:19 -0300

    feat: add notification icon

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,8 +37,20 @@ const showingNavigationDropdown = ref(false);
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
                             <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
+                            <div class="ms-3 relative inline-flex">
+
+                                <button
+                                    type="button"
+                                    class="relative ml-auto mr-5 shrink-0 rounded-full p-1 text-gray-500 hover:text-gray-800 focus:ring-2 focus:ring-gray-500  focus:outline-hidden"
+                                >
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">View notifications</span>
+                                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                                    </svg>
+                                </button>
+
+                                <Dropdown align="right" width="48" >
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
                                             <button
