839aa12b78d1744819f93dfee3203c1e9f2d92bc
leandro <leandro@pop-os.localdomain>
2025-06-01 16:41:11 -0300

    style: register page

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -5,6 +5,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
 const form = useForm({
     name: '',
@@ -24,7 +25,103 @@ const submit = () => {
     <GuestLayout>
         <Head title="Register" />
 
-        <form @submit.prevent="submit">
+        <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <ApplicationLogo class="mb-12" />
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" @submit.prevent="submit">
+
+                    <div>
+                        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+
+                        <input
+                            id="name"
+                            type="text"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            v-model="form.name"
+                            required
+                            autofocus
+                            autocomplete="name"
+                        />
+                    </div>
+
+                    <div class="mt-2">
+                        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
+                        <input
+                            v-model="form.email"
+                            type="email"
+                            name="email"
+                            id="email"
+                            autocomplete="email"
+                            required=""
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                            <div class="text-sm">
+                                <Link
+                                    v-if="canResetPassword"
+                                    :href="route('password.request')"
+                                    class="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Forgot password?
+                                </Link>
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <input
+                                v-model="form.password"
+                                type="password"
+                                name="password"
+                                id="password"
+                                autocomplete="current-password"
+                                required=""
+                                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+
+                        <div class="mt-6">
+                            <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
+
+                            <input
+                                id="password_confirmation"
+                                type="password"
+                                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                v-model="form.password_confirmation"
+                                required
+                                autocomplete="new-password"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                        </div>
+                    </div>
+
+                    <div class="mt-6">
+                        <button
+                            type="submit"
+                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Register
+                        </button>
+                    </div>
+                </form>
+
+                <p class="mt-10 text-center text-sm/6 text-gray-500">
+                    Already a member?
+                    {{ ' ' }}
+                    <Link
+                        :href="route('login')"
+                        class="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                        Log in!
+                    </Link>
+                </p>
+            </div>
+        </div>
+
+     <!--    <form @submit.prevent="submit">
             <div>
                 <InputLabel for="name" value="Name" />
 
@@ -98,6 +195,6 @@ const submit = () => {
                     Register
                 </PrimaryButton>
             </div>
-        </form>
+        </form> -->
     </GuestLayout>
 </template>
