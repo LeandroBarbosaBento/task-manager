66acc8b12622ad62dbebc4d9122a54c6f12765f0
leandro <leandro@pop-os.localdomain>
2025-05-31 23:49:16 -0300

    feat: init task view

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,18 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const people = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  }
+]
 </script>
 
 <template>
@@ -13,10 +25,30 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                <div class="bg-white overflow-hidden sm:rounded-lg">
+                    <ul
+                        role="list"
+                        class="divide-y divide-gray-100 border-t border-gray-200 bg-white p-2 px-12"
+                    >
+                        <li v-for="person in people" :key="person.email" class="flex justify-between gap-x-6 py-2">
+                            <div class="flex min-w-0 gap-x-4">
+                                <div class="min-w-0 flex-auto">
+                                    <p class="text-sm/6 font-semibold text-gray-900">
+                                        My task description
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                                <p class="text-sm/6 text-gray-900">
+                                    Insert the checkbox
+                                </p>
+
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
+
         </div>
     </AuthenticatedLayout>
 </template>
