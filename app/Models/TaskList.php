009a4397de56551f2c773d40926f9ae12ec5affc
leandro <leandro@pop-os.localdomain>
2025-06-01 20:50:10 -0300

    feat: create tasks and task lists models, controllers and migrations

---
+++ app/Models/TaskList.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class TaskList extends Model
+{
+    use HasFactory;
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
